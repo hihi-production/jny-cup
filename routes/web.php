9dc1ce2e116e1496614c42fc89f52908ed013879
akhmadrizki <akhmadrizki24@gmail.com>
2024-01-13 10:13:01 +0800

    chore: add auth package

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.dashboard.base');
 });
 
 Route::get('dashboard-login', [LoginController::class, 'index'])->name('admin');
