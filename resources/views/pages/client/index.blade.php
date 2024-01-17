51d2c95f4255d5a99b39163aea37b4d26956c4f1
akhmadrizki <akhmadrizki24@gmail.com>
2024-01-17 22:45:52 +0800

    chore: main page

--- resources/views/pages/client/index.blade.php
+++ resources/views/pages/client/index.blade.php
@@ -3,15 +3,53 @@
 @section('additional-css')
     <style scoped>
         .container {
-            max-width: 540px;
+            padding: 0 !important;
+        }
+        .btn-position {
+            left: 25%;
+            top: 27%;
+        }
+        .pushable {
+            background: #bcf352;
+            border-radius: 12px;
+            border: none;
+            padding: 0;
+            cursor: pointer;
+            outline-offset: 4px;
+        }
+        .front {
+            display: block;
+            padding: 6px 42px;
+            border-radius: 12px;
+            font-size: 1.25rem;
+            background: #fcfef0;
+            color: #4ba74f;
+            font-weight: bold;
+            transform: translateY(-6px);
+        }
+
+        .pushable:active .front {
+            transform: translateY(-2px);
+        }
+        @media (min-width: 414px) {
+            .btn-position {
+                left: 30%;
+                top: 27%;
+            }
+        }
+
+        @media (min-width: 576px) {
+            .container {
+                max-width: 670px;
+            }
+            .btn-position {
+                left: 38%;
+                top: 27%;
+            }
         }
         .image-cover {
             background-size: cover;
-            object-fit: cover;
-        }
-        .btn-position {
-            bottom: 0;
-            left: 0;
+            object-fit: fill;
         }
     </style>
 @endsection
@@ -19,8 +57,12 @@
 @section('content')
     <div class="container vh-100">
         <div class="w-100 h-100 position-relative">
-            <img src="{{ asset('client/assets/img/base.png') }}" class="w-100 h-100 image-cover" alt="">
-            <button class="btn btn-success position-absolute btn-position">Start</button>
+            <img src="{{ asset('client/assets/img/base.png') }}" class="w-100 h-100 image-cover" alt="jny-cup">
+            <button class="pushable position-absolute btn-position">
+                <span class="front">
+                  Start Now
+                </span>
+              </button>
         </div>
     </div>
 @endsection
